bf6e69010b84d941e96a8e21a86c02b9f4eb69a0
Chrispy <christiancarteno@hotmail.com>
2023-11-12 23:54:35 -0800

    capstone project layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      Some random text that is a placeholder for the app
+      
     </div>
   );
 }
+
+function Restaurant() {
+  return (
+    <div>
+      <header>
+        <h1>Restaurant Name</h1>
+      </header>
+      <img src="restaurant-image.jpg" alt="Restaurant" />
+      <section>
+        <h2>Menu</h2>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </section>
+      <section>
+        <h2>Reviews</h2>
+        <ul>
+          <li>Review 1</li>
+          <li>Review 2</li>
+          <li>Review 3</li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default Restaurant;
 
 export default App;
